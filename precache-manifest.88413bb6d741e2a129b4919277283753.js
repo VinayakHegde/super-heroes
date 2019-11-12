0b044ea3f4476488138211a63a493896e1611aa3
Vinayak Hegde <vinayak.nandi@gmail.com>
2019-11-12 23:50:03 +0000

    Updates

---
+++ precache-manifest.88413bb6d741e2a129b4919277283753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea4088b04f3c609895e680a10b2499a",
+    "url": "/super-heroes/index.html"
+  },
+  {
+    "revision": "d65a3cc70cb302ac3a99",
+    "url": "/super-heroes/static/css/main.f555d261.chunk.css"
+  },
+  {
+    "revision": "d82eb26bb4cd7332f068",
+    "url": "/super-heroes/static/js/2.62398c5c.chunk.js"
+  },
+  {
+    "revision": "d65a3cc70cb302ac3a99",
+    "url": "/super-heroes/static/js/main.b33f7dd4.chunk.js"
+  },
+  {
+    "revision": "b11be68342f43d009521",
+    "url": "/super-heroes/static/js/runtime-main.7137857a.js"
+  }
+]);
